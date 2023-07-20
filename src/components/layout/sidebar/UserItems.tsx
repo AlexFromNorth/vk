ae95ea1af972971ea4f89885555de3539b90b1a4
AlexFromNorth <jacklex.ab@gmail.com>
2023-07-20 18:00:55 +0600

    added userItems

--- src/components/layout/sidebar/UserItems.tsx
+++ src/components/layout/sidebar/UserItems.tsx
@@ -13,6 +13,37 @@ import { Link } from "react-router-dom";
 import { QuestionAnswer } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
+const users = [
+  {
+    id: 0,
+    avatar:
+      "https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg",
+    name: "Alexander Biryukov",
+    isInNetwork: true,
+  },
+  {
+    id: 1,
+    avatar:
+      "https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg",
+    name: "Alexander Biryukov",
+    isInNetwork: true,
+  },
+  {
+    id: 2,
+    avatar:
+      "https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg",
+    name: "Alexander Biryukov",
+    isInNetwork: false,
+  },
+  {
+    id: 3,
+    avatar:
+      "https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg",
+    name: "Alexander Biryukov",
+    isInNetwork: false,
+  },
+];
+
 const UserItems: FC = () => {
   const navigate = useNavigate();
 
@@ -26,43 +57,47 @@ const UserItems: FC = () => {
         backgroundColor: "#F1F7FA",
       }}
     >
-      <Link
-        to={"/profile"}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          color: "#111",
-          marginBottom: 12,
-        }}
-      >
-        <Box
-          sx={{
-            position: "relative",
-            marginRight: 2,
-            width: 50,
-            height: 50,
+      {users.map((user) => (
+        <Link key={user.id}
+          to={`/profile/${user.id}`}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "#111",
+            marginBottom: 12,
           }}
         >
-          <Avatar
-            alt="avatar"
-            src="https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg"
-            sx={{ width: 50, height: 50, borderRadius: "50%" }}
-          />
           <Box
             sx={{
-              background: "#4FB14F",
-              width: 13,
-              height: 13,
-              position: "absolute",
-              bottom: 0,
-              right: 0,
-              borderRadius: "50%",
-              border: "2px solid #F1F7FA",
+              position: "relative",
+              marginRight: 2,
+              width: 50,
+              height: 50,
             }}
-          />
-        </Box>
-        <span style={{ fontSize: 14 }}>Alex Biryukov</span>
-      </Link>
+          >
+            <Avatar
+              alt="avatar"
+              src={user.avatar}
+              sx={{ width: 50, height: 50, borderRadius: "50%" }}
+            />
+            {!!user.isInNetwork && (
+              <Box
+                sx={{
+                  background: "#4FB14F",
+                  width: 13,
+                  height: 13,
+                  position: "absolute",
+                  bottom: 0,
+                  right: 0,
+                  borderRadius: "50%",
+                  border: "2px solid #F1F7FA",
+                }}
+              />
+            )}
+          </Box>
+          <span style={{ fontSize: 14 }}>{user.name}</span>
+        </Link>
+      ))}
       <List sx={{ display: "inline" }}>
         <ListItem disablePadding>
           <ListItemButton onClick={() => navigate("/messages")}>
