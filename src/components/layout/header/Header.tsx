304e8d253f3752864aec0145ed3adc75d4e85b38
AlexFromNorth <jacklex.ab@gmail.com>
2023-07-23 22:59:15 +0600

    added Header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,9 +1,22 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from "react";
+import logoImg from "./vk_logo.png";
+import { Search } from "@mui/icons-material";
+import styles from "./Header.module.css";
 
-const Header:FC = () => {
+const Header: FC = () => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <div className={styles.imageWrapper}>
+        <img src={logoImg} alt="VK" />
+      </div>
+      <div className={styles.wrapper}>
+        {!isSearchActive && <Search />}
+        <input type="text" placeholder="Search" onClick={()=>setIsSearchActive(true)}/>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
