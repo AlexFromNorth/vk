8a41d41f863571cff94546d81c994f9f4cce3b09
AlexFromNorth <jacklex.ab@gmail.com>
2023-07-25 19:10:15 +0600

    added registration in firebase

--- src/components/pages/auth/Auth.tsx
+++ src/components/pages/auth/Auth.tsx
@@ -1,61 +1,86 @@
-import { Button, ButtonGroup, Grid, TextField } from "@mui/material";
+import { Alert, Button, ButtonGroup, Grid, TextField } from "@mui/material";
 import React, { FC, SyntheticEvent, useState } from "react";
 import { IUserData } from "./types";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 const Auth: FC = () => {
   const [isRegForm, setIsRegForm] = useState(false);
-
+  const [error, setError] = useState("");
   const [userData, setUserData] = useState<IUserData>({
     email: "",
     password: "",
   } as IUserData);
 
-  const handleLogin = (e: SyntheticEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const auth = getAuth();
+
     if (isRegForm) {
-      console.log("register");
+      try {
+        await createUserWithEmailAndPassword(
+          auth,
+          userData.email,
+          userData.password
+        );
+      } catch (error: any) {
+        error.message && setError(error.message);
+      }
     } else {
       console.log("auth");
     }
     console.log(userData.email, userData.password);
     setUserData({
-      email: '',
-      password: '',
-    })
+      email: "",
+      password: "",
+    });
   };
 
   return (
-    <Grid display='flex' justifyContent='center' textAlign='center'>
-      <form onSubmit={handleLogin}>
-        <TextField
-          type="email"
-          label="Email"
-          variant="outlined"
-          value={userData.email}
-          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-          sx={{ display: "block", marginBottom: 3 }}
-          required
-        />
-        <TextField
-          type="password"
-          label="Password"
-          variant="outlined"
-          value={userData.password}
-          onChange={(e) =>
-            setUserData({ ...userData, password: e.target.value })
-          }
-          sx={{ display: "block", marginBottom: 3 }}
-          required
-        />
-        <ButtonGroup
-          variant="contained"
-          aria-label="outlined primary button group"
-        >
-          <Button type="submit" onClick={() => setIsRegForm(false)}>Auth</Button>
-          <Button type="submit" onClick={() => setIsRegForm(true)}>Register</Button>
-        </ButtonGroup>
-      </form>
-    </Grid>
+    <>
+      {error && (
+        <Alert severity="error" style={{ marginBottom: 20 }}>
+          {error}
+        </Alert>
+      )}
+      <Grid display="flex" justifyContent="center" textAlign="center">
+        <form onSubmit={handleLogin}>
+          <TextField
+            type="email"
+            label="Email"
+            variant="outlined"
+            value={userData.email}
+            onChange={(e) =>
+              setUserData({ ...userData, email: e.target.value })
+            }
+            sx={{ display: "block", marginBottom: 3 }}
+            required
+          />
+          <TextField
+            type="password"
+            label="Password"
+            variant="outlined"
+            value={userData.password}
+            onChange={(e) =>
+              setUserData({ ...userData, password: e.target.value })
+            }
+            sx={{ display: "block", marginBottom: 3 }}
+            required
+          />
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button type="submit" onClick={() => setIsRegForm(false)}>
+              Auth
+            </Button>
+            <Button type="submit" onClick={() => setIsRegForm(true)}>
+              Register
+            </Button>
+          </ButtonGroup>
+        </form>
+      </Grid>
+    </>
   );
 };
 
