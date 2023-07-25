c49f30c8e6d8f29e118abee642487d80deccd731
AlexFromNorth <jacklex.ab@gmail.com>
2023-07-25 16:53:36 +0600

    added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './components/layout/Layout';
 import RoutesItem from './components/routes/RoutesItem';
+import firebase from 'firebase/compat/app'; 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAphBHSmUNvA8VUz9ZK2GHc24ZiFffIyBk",
+  authDomain: "vk-petproject.firebaseapp.com",
+  projectId: "vk-petproject",
+  storageBucket: "vk-petproject.appspot.com",
+  messagingSenderId: "451444827001",
+  appId: "1:451444827001:web:42bcf08d926b4536a4c5a8",
+  measurementId: "G-RP9NT9NJZN"
+}
+
+firebase.initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(
